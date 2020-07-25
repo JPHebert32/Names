233f9a304dc63e13206adfa03ac30299f81369b7
JP Hebert <JPHebert32@gmail.com>
2020-07-25 14:30:38 -0600

    api

--- app.py
+++ app.py
@@ -15,11 +15,10 @@ engine = create_engine('postgresql://postgres:JPH401@mc@localhost:5432/Names_DB'
 
 male_year_table = pd.read_sql("select year_of_birth.year, year_of_birth.name, year_of_birth.sex, year_of_birth.count From public.year_of_birth WHERE year_of_birth.sex = 'M'", engine, index_col=None)
 #print(male_year_table)
-
 female_year_table = pd.read_sql("select year_of_birth.year, year_of_birth.name, year_of_birth.sex, year_of_birth.count From public.year_of_birth WHERE year_of_birth.sex = 'F'", engine, index_col=None)
 #print(female_year_table)
 
-#al_table = pd.read_sql("SELECT state_names.year,state_names.name, state_name.sex, state_name.count, state_name.state FROM public.state_name WHERE state_name.state = 'AL'",engine, index_col=None)
+
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -47,25 +46,34 @@ def names():
     return (f"Welcome to United States Name API<br/>"
             f"-----------------------------------------------<br/>"
             f"List of availible routes:<br/>"
-            f"/api/v1.0/years-male<br/>"
-            f"/api/v1.0/years-female<br/>"
-            f"/api/v1.0/AL<br/>"
-            f"/api/v1.0/AK<br/>"
-            f"/api/v1.0/AZ<br/>"
-            f"/api/v1.0/AR<br/>"
+            f"/api/v1.0/years/<sex><br/>"
+            f"/api/v1.0/states/<state><br/>"
+            f"-----------------------------------------------<br/>"
             )
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-@app.route("/api/v1.0/years-male")
-def years_male():
-    return(male_year_table.to_json(orient="records"))
+@app.route(f"/api/v1.0/years/<sex>")
+def years(sex):
 
-@app.route("/api/v1.0/years-female")
-def years_female():
-    return(female_year_table.to_json(orient="records"))
+    if sex != 'M' and sex != 'F':
+        return "error"
+    print(sex)
+    query = f"select year_of_birth.year, year_of_birth.name, year_of_birth.sex, year_of_birth.count From public.year_of_birth WHERE year_of_birth.sex = '{sex}';"
+    sex_year_table = pd.read_sql(query, engine, index_col=None)
+    return(sex_year_table.to_json(orient="records"))
+
+@app.route("/api/v1.0/states/<state>")
+def state(state):
+    #if sex != 'M' or sex!= 'F':
+    #    return "error"
+
+    query = f"select state_names.year, state_names.name, state_names.sex, state_names.count From public.state_names WHERE state_names.state = '{state}';"
+    state_table = pd.read_sql(query, engine, index_col=None)
+    return(state_table.to_json(orient="records"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
