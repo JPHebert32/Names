5f0e5a9a3c45df4a5e10ba53ded4ce16b5fe9936
JP Hebert <JPHebert32@gmail.com>
2020-07-26 13:09:40 -0600

    variable data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,22 @@
-//Search function -User Input
+// Search function -User Input
 // Submit Button handler
-// function handleSubmit() {
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
-//
-//   // Select the input value from the form
-//   var name = d3.select("#nameInput").node().value;
-//   console.log(name);
-// }
+function handleSubmit() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
+  // Select the input value from the form
+  var selectedName = d3.select("#nameInput").node().value;
+  console.log(`Selected Name: ${selectedName}`);
 
-
+  // // clear the input value
+  // d3.select("#stockInput").node().value = "";
+  //
+  // // Build the plots and map with selectedName
+  // create Map(selectedName);
+  // popTable(selectedName);
+}
+// Add event listener for submit button
+d3.select("#submit").on("click", handleSubmit);
 
 // Map Visual
 // Create a map object
@@ -25,57 +31,60 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "mapbox/light-v10",
   accessToken: API_KEY
 }).addTo(myMap);
 
 
 
-// //Table Visual
-//
-// // Assign the data to a descriptive variable
-// var yrMaleURL = "http://127.0.0.1:5000/api/v1.0/years/M";
-// var yrFemaleURL = "http://127.0.0.1:5000/api/v1.0/years/F";
-//
-//
-// // Get a reference to the table body
-// var tbody = d3.select('tbody');
-//
-// // Populate Table Function
-// function popTable(yrMaleURL, yrFemaleURL) {
-//   //Clear Existing Table
-//   tbody.html('');
-//   //Use d3 to update each cell's text with
-//   nameData.forEach((names) => {
-//     // Append table header from data
-//     var row = tbody.append('tr');
-//     //Use `Object.entries` data value
-//     Object.entries(names).forEach(([key,value]) => {
-//       // Append a cell to the row for each value
-//       var cell = row.append('td');
-//       cell.text(value);
-// });
-// });
-// }
-//
-//
-// // Filter Table by enter date Function
-// function handleClick() {
-//   let nameValue  = d3.select('#nameInput').property('value');
-//   //Filter Data
-//   let filteredData = tableData.filter(row => row.name === nameValue);
-//   popTable(filteredData);
-//   console.log(filteredData);
-// }
-//
-// // Store value from form
-// var submit = d3.select("#filter-btn").on("click", handleClick);
-//
-//
-//
-// // Populate the Table
-// popTable(yrMaleURL);
-// popTable(yrFemaleURL);
-//
-// // Add event listener for submit button
-// d3.select("#submit").on("click", handleSubmit);
+//Table Visual
+
+// Assign the data to a descriptive variable
+var yrMaleURL = "http://127.0.0.1:5000/api/v1.0/years/M";
+var yearsMaleData = []
+d3.json(yrMaleURL).then(function(maleData) {
+  yearsMaleData = maleData;
+})
+var yrFemaleURL = "http://127.0.0.1:5000/api/v1.0/years/F";
+var yearsFemaleData = []
+d3.json(yrFemaleURL).then(function(femaleData) {
+  yearsFemaleData = femaleData;
+})
+
+// Get a reference to the table body
+var tbody = d3.select('tbody');
+
+// Populate Table Function
+function popTable(data) {
+  //Clear Existing Table
+  tbody.html('');
+  //Use d3 to update each cell's text with
+  data.forEach((names) => {
+    // Append table header from data
+    let row = tbody.append('tr');
+    //Use `Object.entries` data value
+    Object.values(names).forEach((value) => {
+      // Append a cell to the row for each value
+      let cell = row.append('td');
+      cell.text(value);
+});
+});
+}
+
+
+// Filter Table by enter date Function
+function handleClick() {
+  let nameValue  = d3.select('#nameInput').property('value');
+  //Filter Data
+  let filteredData = tableData.filter(row => row.name === nameValue);
+  popTable(filteredData);
+  console.log(filteredData);
+}
+
+// Store value from form
+var submit = d3.select("#filter-btn").on("click", handleClick);
+
+
+
+// Populate the Table
+popTable(yearsMaleData.slice(1,11));
